7aab2c87be6301d277df0ee210c88cd6635768bd
Odiljon Olimjonov <odiljon.o@payme.uz>
2025-05-05 16:24:18 +0500

    Vid: Простейший сервер

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	fmt.Println("Starting server on :8081")
+	http.ListenAndServe(":8081", nil)
 }
