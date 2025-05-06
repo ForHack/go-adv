e97df6a9141859f23d5ca2224a1f4bf180f94216
Odiljon Olimjonov <odiljon.o@payme.uz>
2025-05-06 11:15:46 +0500

    Vid: 6.5 Упражнение модуль авторизации

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"go/adv-demo/internal/auth"
 	"net/http"
 )
 
 func main() {
+	// conf := configs.LoadConfig()
 	router := http.NewServeMux()
-	NewHelloHandler(router)
+	auth.NewAuthHandler(router)
 
 	server := http.Server{
 		Addr:    ":8081",
